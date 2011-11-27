8f32ee67b2622c4315219cf30b443ab8445bf1f5
Ingo Düppe <ingo.dueppe@dueppe.com>
2011-11-27 22:55:27 +0100

    enhanced

---
+++ examples/distribution-example/src/main/java/de/tutego/maven/distribution/App.java
@@ -0,0 +1,13 @@
+package de.tutego.maven.distribution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ examples/ejb-jboss-simple/src/main/java/de/crowdcode/maven/DataRepository.java
@@ -0,0 +1,19 @@
+package de.crowdcode.maven;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * This is the CDI {@link Qualifier} that can be used to determine what objects
+ * qualify for which injection points. For this application, it is used to
+ * qualify the {@link EntityManager} injection point.
+ * 
+ */
+@Qualifier
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DataRepository {}
