6e343bc628eb2fa618fa450f787015b89133515f
Ingo Düppe <ingo.dueppe@dueppe.com>
2011-11-29 14:54:50 +0100

    archetype customer properties example

---
+++ examples/project-pattern-architype/src/main/resources/archetype-resources/src/main/java/utils/Util.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class Util 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven!" );
+    }
+}
