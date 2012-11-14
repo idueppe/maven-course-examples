58775fdf43994032bdc49213762146240972a5b5
idueppe <ingo.dueppe@dueppe.com>
2012-11-14 09:53:08 +0100

    Add jaxb fluent api code generation

--- examples/jaxb-code-generation/src/test/java/course/freedb/xml/JAXBTest.java
+++ examples/jaxb-code-generation/src/test/java/course/freedb/xml/JAXBTest.java
@@ -21,7 +21,6 @@ import org.junit.Test;
 import course.freedb.domain.Album;
 import course.freedb.domain.Dictionary;
 import course.freedb.domain.ObjectFactory;
-import course.freedb.domain.Track;
 
 public class JAXBTest {
 
@@ -29,74 +28,96 @@ public class JAXBTest {
 	public void testResource() {
 		assertNotNull(getClass().getClassLoader().getResourceAsStream("freedb.xml"));
 	}
-	
+
 	@Test
 	public void testUnmarschallXML() {
 		try {
-			JAXBContext context = JAXBContext.newInstance("course.freedb.domain");
+			JAXBContext context = JAXBContext
+					.newInstance("course.freedb.domain");
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream inputStream = getClass().getClassLoader().getResourceAsStream("freedb.xml");
-			
+			InputStream inputStream = getClass().getClassLoader()
+					.getResourceAsStream("freedb.xml");
+
 			@SuppressWarnings("unchecked")
-			JAXBElement<Dictionary> element = (JAXBElement<Dictionary>) unmarshaller.unmarshal(inputStream);
+			JAXBElement<Dictionary> element = (JAXBElement<Dictionary>) unmarshaller
+					.unmarshal(inputStream);
 			Dictionary dictionary = element.getValue();
-			
+
 			assertNotNull(dictionary);
 			assertFalse(dictionary.getAlbums().isEmpty());
 			Album album = dictionary.getAlbums().get(0);
 			assertNotNull(album.getName());
 			assertFalse(dictionary.getAlbums().get(0).getTracks().isEmpty());
-		
+
 		} catch (JAXBException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testMarshallXML() {
-		Dictionary dictionary = generateDumyStructure();
-		
+//		Dictionary dictionary = generateDummyStructure();
+		Dictionary dictionary = generateFluentStructure();
+
 		String outputXml = "";
 		try {
-			JAXBContext context = JAXBContext.newInstance("course.freedb.domain");
+			JAXBContext context = JAXBContext
+					.newInstance("course.freedb.domain");
 			Marshaller marshaller = context.createMarshaller();
 			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			
+
 			Writer writer = new StringWriter();
 			ObjectFactory objectFactory = new ObjectFactory();
-			JAXBElement<Dictionary> element = objectFactory.createDictionary(dictionary);
-			
+			JAXBElement<Dictionary> element = objectFactory
+					.createDictionary(dictionary);
+
 			marshaller.marshal(element, writer);
-			
+
 			outputXml = writer.toString();
 			writer.close();
-			
+
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
-		
+
 		System.out.println(outputXml);
 		assertTrue(outputXml != null && !outputXml.isEmpty());
-		
+
 	}
 
-	private Dictionary generateDumyStructure() {
-		Dictionary dictionary = new Dictionary();
-		Album album = new Album();
-		album.setArtist("Artist");
-		album.setDiscId("discID");
-		album.setGenre("genre");
-		album.setYear(12345);
-		dictionary.getAlbums().add(album);
-		Track track = new Track();
-		track.setTitle("title");
-		track.setTrackNo(1);
-		album.getTracks().add(track);
-		return dictionary;
+	private ObjectFactory factory = new ObjectFactory();
+
+//	private Dictionary generateDummyStructure() {
+//		Dictionary dictionary = factory.createDictionary();
+//		Album album = factory.createAlbum();
+//		album.setArtist("Artist");
+//		album.setDiscId("discID");
+//		album.setGenre("genre");
+//		album.setYear(12345);
+//		dictionary.getAlbums().add(album);
+//		Track track = factory.createTrack();
+//		track.setTitle("title");
+//		track.setTrackNo(1);
+//		album.getTracks().add(track);
+//		return dictionary;
+//	}
+
+	private Dictionary generateFluentStructure() {
+		return factory.createDictionary()
+			.withAlbums(
+				factory.createAlbum()
+					.withArtist("Artist")
+					.withDiscId("discId")
+					.withGenre("genre")
+					.withYear(12345)
+					.withTracks(
+						factory.createTrack()
+							.withTitle("title")
+							.withTrackNo(1)));
 	}
-	
+
 }
