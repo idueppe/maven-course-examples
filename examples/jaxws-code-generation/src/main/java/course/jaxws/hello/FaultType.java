021a21f386b1e3575de2c3c856dfc795e135cfad
Torsten <torsten.friebe@googlemail.com>
2014-10-22 19:59:31 +0200

    submodules jaxws-code-generation and jaxws-fraction-service put back in service

---
+++ examples/jaxws-code-generation/src/main/java/course/jaxws/hello/FaultType.java
@@ -0,0 +1,31 @@
+package course.jaxws.hello;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "FaultType")
+public class FaultType {
+	
+	@XmlAttribute
+	private String msg;
+
+	public FaultType() {
+		
+	}
+	
+	public FaultType(String msg) {
+		this.msg = msg;
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
